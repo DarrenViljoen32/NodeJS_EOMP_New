4067762753391d30937fe58432baa54e615c15bd
JsonSept <jasonseptember.za@gmail.com>
2024-02-26 16:16:57 +0200

    updates

--- backend/models/database.js
+++ backend/models/database.js
@@ -16,7 +16,6 @@ const getProduct = async()=>{
     `)
     return result
 }
-
 const getSingle = async(prodID) =>{
     const [result] = await pool.query(`
     SELECT *
@@ -50,5 +49,51 @@ const editProduct = async(prodName,amount,Category,prodID)=>{
     return item
 }
 
+//users
+const getUsers = async()=>{
+    const[result] = await pool.query(`
+    SELECT * FROM
+    users
+    `)
+    return result
+}
 
+const getSingleUser = async(userID) =>{
+    const [result] = await pool.query(`
+    SELECT *
+    FROM users
+    WHERE userID = ?
+    `,[userID])
+    return result
+}
+const deleteUser = async(userID) => { // pool helps connect to the database
+    const [result] = await pool.query(` 
+        DELETE FROM users WHERE (userID) = (?)
+    `,[userID])
+    return result
+}
+
+const addUser = async(firstName, lastName, userAge, Gender, userRole, emailAdd, userPass, userProfile) => {
+    const [user] = await pool.query(`
+    INSERT INTO users (firstName, lastName, userAge, Gender, userRole, emailAdd, userPass, userProfile) VALUES (?,?,?,?,?,?,?,?)
+    `,[firstName, lastName, userAge, Gender, userRole, emailAdd, userPass, userProfile])
+    return getUsers(user)
+}
+
+const checkUser = async (emailAdd) =>{
+    const [[{password}]] = await pool.query(`
+    SELECT password FROM users WHERE emailAdd = ?
+    `,[emailAdd])//prepared statement
+    return password
+}
+
+const editUser = async(firstName, lastName, userAge, Gender, userRole, emailAdd, userPass, userProfile,userID)=>{
+    const [user] = await pool.query(`
+        UPDATE users
+        SET firstName=?, lastName=?, userAge=?, Gender=?,userRole=?, emailAdd=?, userPass=?, userProfile=?
+        WHERE (userID=?)
+    `,[firstName, lastName, userAge, Gender, userRole, emailAdd, userPass, userProfile, userID])
+    return user
+}
+export {getUsers,getSingleUser,deleteUser,addUser,editUser,checkUser}
 export {getProduct,deleteItem,getSingle,addProduct,editProduct}
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,4 @@
 import express from 'express'
-// import {getProduct,getProductID,deleteItem,addProduct,editProduct} from '../models/database.js'
 import controller from '../controller/products.js'
 const router = express.Router()
 
@@ -8,9 +7,11 @@ router
             .get(controller.getProds)
             .post(controller.getPost)
             
+            
 router
     .route('/:id')
             .delete(controller.rmvPost)
             .patch(controller.getPatch)
             .get(controller.getItem)
+
 export default router
